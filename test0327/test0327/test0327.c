0742ce7bc2214a217f72140d137893a7d15ec474
wqwqqq <wqw2995966742@163.com>
2023-04-18 23:47:14 +0800

    tijiao

---
+++ test0327/test0327/test0327.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+//int main()
+//{
+	/*int a=0;*/
+	//if(a%2!=0)
+	//{
+	//	printf("%d\n",a);
+	//}
+	//int a=1;
+	//for(i=0;i<49;i++)
+	//{
+	//	a+=2;
+	//	printf("%d\n",a);
+	//}
+	/*for(a=0;a<100;a++)
+	{
+		a++;
+		if(a%2!=0&&a<100)
+			{
+				printf("%d\n",a);
+			
+			}
+	}*/
+//	int day=0;
+//	scanf("%d",&day);
+//	switch(day)
+//	{
+//		case 1 :printf("星期一\n");break;
+//		case 2 :printf("星期二\n");break;
+//		case 3 :printf("星期三\n");break;
+//		case 4 :printf("星期四\n");break;
+//		case 5 :printf("星期五\n");break;
+//		case 6 :printf("星期六\n");break;
+//		case 7 :printf("星期天\n");break;
+//		default :
+//				printf("输入错误\n");break;
+//
+//	}
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int n=1;
+//	int m=2;
+//	switch (n)
+//	{
+//	case 1:m++;
+//	case 2:n++;
+//	case 3:
+//		switch(n)
+//		{
+//		case 1:n++;
+//		case 2:m++;n++;break;
+//		}
+//	case 4:m++;
+//			break;
+//	default:
+//		break;
+//	}
+//printf("m=%d,n=%d\n",m,n);
+//	return 0;
+//}
+//int main()
+//{
+//	while(1)
+//		printf("hehe\n");
+//}
+//int main()
+//{
+//	int i=1;
+//	while(i<11)
+//	{
+//		if(i==5)
+//			//break;//跳出循环
+//		continue;//后面代码不在执行
+//		printf("%d\n",i);
+//		i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//
+//	return 0;
+//}
