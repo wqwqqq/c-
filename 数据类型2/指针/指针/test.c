7a382bc05204ed40d0ab403daee27a3acf129576
wqwqqq <wqw2995966742@qq.com>
2023-04-21 19:53:15 +0800

    提交代码

---
+++ 数据类型2/指针/指针/test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	char ch = 'w';
+//	char* pc = &ch;
+//
+//	return 0;
+//}
+
+//int mian()
+//{
+//	char arr[] = "abcdef";
+//	char* p = "abcdef";
+//	printf();
+//	return 0;
+//}
+
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "acbdef";
+	char* p1 = "abcdef";
+	char* p2 = "abcdef";
+	if (p1==p2)
+	{
+		printf("hehe\n");
+	}
+	else
+	{
+		printf("haha\n");
+	}
+	return 0;
+}
